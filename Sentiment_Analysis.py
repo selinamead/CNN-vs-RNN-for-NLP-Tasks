f86b8fb2c670c1823d7cb61dce7bb9fac55a93a9
Selina Mead <selina@Selinas-MacBook-Pro.local>
2019-09-23 12:21:59 +0200

    Add Sentiment Analysis Class

---
+++ Sentiment_Analysis.py
@@ -0,0 +1,17 @@
+''' Sentiment Analysis of Movie Reviews using CNN, RNN and LSTM architecture '''
+
+def get_data():
+	# Read file from csv
+	return data_set
+
+def pre_process(data):
+	# Data cleaning
+	# Tokenise
+	# Split data into train test
+	return X_train, y_train, X_test, y_test
+
+def CNN():
+
+def RNN():
+
+def LSTM():
